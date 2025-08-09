1379558236d21b1138170123332ada2d62cd9883
harrison <harrisonpayesa@gmail.com>
2025-08-09 18:36:36 -0500

    Envio de emails funcionando

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import smtplib
 from app_config import *
 
-conexion = smtplib.SMTP("smtp.gmail.com") # Hacer la conexion al servidor SMTP del proveedor del correo
+conexion = smtplib.SMTP("smtp.gmail.com", port=587) # Hacer la conexion al servidor SMTP del proveedor del correo, se incluye el puerto por temas de actualizacion del SMTP
 conexion.starttls() # Encriptar comunicacion
-conexion.login(user= EMAIL_ADRESS, pasword= PASSWORD) # Iniciamos sesion en el correo
-conexion.sendmail(from_addr= EMAIL_ADRESS, to_addrs=EMAIL_ADRESS, msg= "Hello") # Enviar el correo a la direccion especificada
+conexion.login(user= EMAIL_ADRESS, password= PASSWORD) # Iniciamos sesion en el correo
+conexion.sendmail(from_addr= EMAIL_ADRESS, to_addrs=EMAIL_DESTINATARY, msg= "Subject:Hello\nHow are you?") # Enviar el correo a la direccion especificada
 conexion.close() # Cerramos la conexion
